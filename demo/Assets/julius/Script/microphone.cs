f662e08863f6ef66f5849d10bfe75b5811e2e8d8
oitsmalab <oitsmalab@gmail.com>
2015-07-21 15:59:00 +0900

    デモアプリ　都道府県を辞書に登録

---
+++ demo/Assets/julius/Script/microphone.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class microphone : MonoBehaviour {
+
+	public float vol = 0;
+	public float[] spectrum;
+	public float max = 1;
+	public float min = 1;
+
+
+	private float GetAveragedVolume()
+	{ 
+		float[] data = new float[256];
+		float a = 0;
+		GetComponent<AudioSource>().GetOutputData(data,0);
+		
+		foreach(float s in data)
+		{
+			a += Mathf.Abs(s);
+		}
+		
+		return a / 256;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource>().clip = Microphone.Start(null, true, 999, 44100);  // マイクからのAudio-InをAudioSourceに流す
+		GetComponent<AudioSource>().loop = true;                                      // ループ再生にしておく
+		GetComponent<AudioSource>().mute = true;                                      // マイクからの入力音なので音を流す必要がない
+		while (!(Microphone.GetPosition("") > 0)){}             // マイクが取れるまで待つ。空文字でデフォルトのマイクを探してくれる
+		GetComponent<AudioSource>().Play(); 
+
+		spectrum = new float[1024];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		spectrum = GetComponent<AudioSource>().GetSpectrumData(1024, 0, FFTWindow.BlackmanHarris);
+		vol = GetAveragedVolume();
+
+		if(vol > max){
+			max = vol;
+		}
+		if(vol < min){
+			min = vol;
+		}
+
+	}
+}
